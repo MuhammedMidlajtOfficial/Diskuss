d6625dd6f8819fc7daac63ff0642365f68c37eaf
rajanit_navapara9 <rajanitnavapara9999@gmail.com>
2024-10-26 12:04:44 +0530

    Updated Subscription

---
+++ src/Routes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const authIndividualRouter = require('./Individual/authIndividualRouter.js')
+const authEnterpriseRouter = require('./Enterprise/authEnterpriseRouter.js')
+const profileRoutes = require('./Profile/profileRoutes.js')
+const subscriptionRouter = require('./Subscription/SubscriptionRouter.js')
+
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/individual',
+    route: authIndividualRouter,
+  },
+  {
+    path: '/enterprise',
+    route: authEnterpriseRouter,
+  },
+  {
+    path: '/profile',
+    route: profileRoutes,
+  },
+  {
+      path: '/subscription',
+      route: subscriptionRouter,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const app = express()
 const {v4 : uuidv4} = require('uuid');
 const session = require('express-session');
 const nocache = require('nocache');
-const authIndividualRouter = require('./Routes/Individual/authIndividualRouter.js')
-const authEnterpriseRouter = require('./Routes/Enterprise/authEnterpriseRouter.js')
-const profileRoutes = require('./Routes/Profile/profileRoutes.js')
+const routes = require('./Routes/index.js')
+// const authIndividualRouter = require('./Routes/Individual/authIndividualRouter.js')
+// const authEnterpriseRouter = require('./Routes/Enterprise/authEnterpriseRouter.js')
+// const profileRoutes = require('./Routes/Profile/profileRoutes.js')
 
 require('dotenv').config();
 
@@ -19,13 +20,16 @@ app.use(express.json());
 
 app.use(express.json({ limit: "10mb" }));
 
-app.use('/individual/',authIndividualRouter)
-app.use('/enterprise/',authEnterpriseRouter)
+app.use('/api/v1', routes);
 
-app.use("/api/profile", profileRoutes);
+// app.use('/individual/',authIndividualRouter)
+// app.use('/enterprise/',authEnterpriseRouter)
+// app.use("/api/profile", profileRoutes);
 
 app.get('/api/',(req,res)=>{
-  res.send("Welcome to Diskuss API")
+  res.send({
+    message : "Welcome to the API"
+  })
 })
 
 const port = process.env.PORT | "3000"
