de7121c04bcedd306e67d75041fa5ed645ec6c49
MidlajLevonTechno <muhammedmidlaj236@gmail.com>
2024-10-29 10:38:58 +0530

    changed socket timeout for debug

--- src/DBConfig.js
+++ src/DBConfig.js
@@ -9,8 +9,8 @@ if (!process.env.MongoDBURL) {
 }
 
 mongoose.connect(process.env.MongoDBURL,{
-  connectTimeoutMS: 20000, 
-  socketTimeoutMS: 45000,
+  connectTimeoutMS: 200000, 
+  socketTimeoutMS: 450000,
 })
   .then(() => {
     console.log('DB Connected');
