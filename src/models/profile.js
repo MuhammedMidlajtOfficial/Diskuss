1383d6e19d2d0e2bb4b0aa73819c5489d8f3f217
MidlajLevonTechno <muhammedmidlaj236@gmail.com>
2024-10-29 15:07:38 +0530

    profile schema changed

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,18 +1,50 @@
 const mongoose = require("mongoose");
 
 const ProfileSchema = new mongoose.Schema({
-  businessName: String,
-  yourName: String,
-  designation: String,
-  mobile: String,
-  email: String,
-  location: String,
-  services: [String], 
-  image: String,       
-  position: String,
-  color: String,
+  businessName: {
+    type:String,
+    required : true
+  },
+  yourName:  {
+    type:String,
+    required : true
+  },
+  designation:  {
+    type:String,
+    required : true
+  },
+  mobile:  {
+    type:String,
+    required : true
+  },
+  email:  {
+    type:String,
+    required : true
+  },
+  location:  {
+    type:String,
+    required : true
+  },
+  services: [ {
+    type:String,
+    required : true
+  }], 
+  image:  {
+    type:String,
+    required : true
+  },       
+  position:  {
+    type:String,
+    required : true
+  },
+  color:  {
+    type:String,
+    required : true
+  },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   cards : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Card', required: false }],
+  referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  incentives : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Incentive', required: false }],
 });
 
 module.exports = mongoose.model("Profile", ProfileSchema);
