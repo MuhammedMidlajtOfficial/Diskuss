348c8943725987fc033d087607e90da68b4021ee
Naren-d-luffy <148849434+Naren-d-luffy@users.noreply.github.com>
2024-11-14 12:36:25 +0530

    Making the enterprise message works (#45)
    * adding cors for io
    
    * fixing bug with socket io
    
    * Creating Chat ID
    
    * changing the modul from user to contact
    
    * debugging something in Message
    
    * debugging message api
    
    * changing the id to user id
    
    * Debugging message
    
    * contact bug fixed
    
    * debugg added
    
    * payment bug fixed
    
    * payment bug fixed
    
    * payment bug fixed
    
    * debugging message
    
    * adding some config in socket io
    
    * making socket io Message API works
    
    * removing names in get all message
    
    * adding new name field
    
    * removing name field
    
    * converting code to support the changes in Database
    
    * changing socket io events
    
    * Adding Enterprise message
    
    * changing something in the socket io
    
    * debugging enterpriseMessage
    
    * correcting the imports
    
    * Making the enterpriseMessage works
    
    ---------
    
    Co-authored-by: Muhammed Midlaj T <117968647+MuhammedMidlajtOfficial@users.noreply.github.com>
    Co-authored-by: MidlajLevonTechno <muhammedmidlaj236@gmail.com>

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const server = http.createServer(app);
 const socketController = require('./Controller/Socketio/socketController.js');
 const messageController = require('./Controller/Message/messageController');
 const groupmessageController = require('./Controller/Message/groupmessageController.js');
+const enterpriseMessageController = require('./Controller/EnterpriseMessage/enterpriseMessageController.js');
 const socketControllers = require('./Controller/Socket.io/NotificationSocketIo.js');
 // const authIndividualRouter = require('./Routes/Individual/authIndividualRouter.js')
 // const authEnterpriseRouter = require('./Routes/Enterprise/authEnterpriseRouter.js')
@@ -39,6 +40,7 @@ const io = socketIo(server, {
 socketController.setSocketIO(io);
 messageController.setSocketIO(io);
 groupmessageController.setSocketIO(io);
+enterpriseMessageController.setSocketIO(io);
 // Initialize SocketController with Socket.io instance
 socketControllers.setSocketIO(io);
 
