76cdf2817fe17008886c716bbdd27f601a029b80
Naren <deadlyassassin2003@gmail.com>
2024-11-14 15:38:31 +0530

    Addind unread message count for enterprise

--- src/Routes/EnterpriseMessage/enterpriseMessageRoute.js
+++ src/Routes/EnterpriseMessage/enterpriseMessageRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const enterpriseMessageController = require("../../Controller/EnterpriseMessage/enterpriseMessageController");
 
 router.post("/sendMessage", enterpriseMessageController.sendMessage); // This should handle POST requests
+router.post("/markEnterpriseMessagesAsRead",enterpriseMessageController.markMessagesAsRead );
 router.get("/", enterpriseMessageController.getMessages); // For getting messages
 
 module.exports = router;
--- src/Routes/Message/messageRoute.js
+++ src/Routes/Message/messageRoute.js
@@ -1,16 +1,3 @@
-// // routes/messageRoutes.js
-// const express = require('express');
-// const { sendMessage,getChatHistory, markAsSeen, } = require('../../Controller/Message/Message');
-// const router = express.Router();
-
-// router.post('/send', sendMessage);
-// router.post('/markAsSeen', markAsSeen);
-// router.get('/chat_History/:chatId', getChatHistory);
-
-
-// module.exports = router;
-
-
 const express = require("express");
 const router = express.Router();
 const messageController = require("../../Controller/Message/messageController");
