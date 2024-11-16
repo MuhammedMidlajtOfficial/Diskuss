89d12c51c3c244ac1188dfe9f0265876e20e359f
Raghu3-3102 <138585033+Raghu3-3102@users.noreply.github.com>
2024-11-16 15:38:36 +0530

    Notification socket io (#57)
    * Adding enterprise meeting api
    
    * Added Analytics
    
    * Updated Analytics
    
    * Updated Analytics
    
    * upating meeting api
    
    * Adding prefrence
    
    * Adding prefrence api
    
    * Socket Io Implementation
    
    ---------
    
    Co-authored-by: rajanit_navapara9 <rajanitnavapara9999@gmail.com>

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const groupmessageController = require('./Controller/Message/groupmessageControl
 const enterpriseMessageController = require('./Controller/EnterpriseMessage/enterpriseMessageController.js');
 const socketControllers = require('./Controller/Socket.io/NotificationSocketIo.js');
 const notificationSocketController = require('./Controller/Socket.io/NotificationSocketIo');
+
 // const authIndividualRouter = require('./Routes/Individual/authIndividualRouter.js')
 // const authEnterpriseRouter = require('./Routes/Enterprise/authEnterpriseRouter.js')
 // const profileRoutes = require('./Routes/Profile/profileRoutes.js')
