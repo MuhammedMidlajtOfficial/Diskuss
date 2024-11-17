c9a91ac8427728aa1f6152a2dce244a5488cbf4a
rajanit_navapara9 <rajanitnavapara9999@gmail.com>
2024-11-17 12:26:51 +0530

    updated user  schema

--- src/models/individualUser.js
+++ src/models/individualUser.js
@@ -18,11 +18,34 @@ const IndividualUserSchema = new mongoose.Schema({
       type: Boolean,
       default: false,
     },
-    referralCode: String,  // Unique referral code
+    referralCode: {type: String},  // Unique referral code
     coins: { type: Number, default: 0 },
     invitedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Referral' }]
     
   }, { timestamps: true });
+
+  //add referral code to user when we register user 
+  IndividualUserSchema.pre('save', async function (next) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let referralCode;
+    let isUnique = false;
+
+    while (!isUnique) {
+        referralCode = Array.from({ length: 6 }, () => characters.charAt(Math.floor(Math.random() * characters.length))).join('');
+
+        const existingUser = await User.findOne({ referralCode });
+        if (!existingUser) {
+            isUnique = true;
+        }
+    }
+    
+    if (!this.referralCode) {
+      const referralCode = Math.random().toString(36).substring(7);
+      this.referralCode = referralCode;
+    }
+    next();
+  });
+
   
   module.exports.individualUserCollection = mongoose.model('User', IndividualUserSchema);
   
