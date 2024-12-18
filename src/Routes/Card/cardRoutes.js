30902f659c3c8d1d3b62d55d4d8441afae188152
Naren <deadlyassassin2003@gmail.com>
2024-12-18 17:44:35 +0530

    Debugging Card Route

--- src/Routes/Card/cardRoutes.js
+++ src/Routes/Card/cardRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const cardController = require("../../Controller/Card/cardController");
-const enterpriseEmployeeController = require('../../Controller/EnterpriseEmployee/enterpriseEmployeeController')
 
 const router = express.Router();
 
-router.get('/:id', enterpriseEmployeeController.getCardForUser)
 router.get("/:id", cardController.getCards);
 router.post("/", cardController.createCard);
 router.patch("/", cardController.updateCard);
