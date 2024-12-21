d5b505bbad9ededd7b693f3df9c24bc22915f333
Rajanit Nagajibhai Navapara <51814458+rajanitnavapara@users.noreply.github.com>
2024-12-21 11:00:04 +0000

    added sla

--- src/services/Ticket/ticketCategory.service.js
+++ src/services/Ticket/ticketCategory.service.js
@@ -1,4 +1,5 @@
 const TicketCategory = require('../../models/ticket/ticketCategoryModel');
+const Ticket = require('../../models/ticket/ticketModel');
 
 exports.create = async (data) => {
     const newCategory = new TicketCategory(data);
@@ -7,19 +8,22 @@ exports.create = async (data) => {
 
 exports.getAll = async () => {
     const categories =  await TicketCategory.find();
-    const stats = []
+    // Map through categories to get stats
+    const stats = await Promise.all(categories.map(async (category) => {
+        const activeCount = await Ticket.countDocuments({ status: 'Open', category: category._id });
+        const solvedCount = await Ticket.countDocuments({ status: 'Resolved', category: category._id });
 
-    for (const category of categories) {
-        const activeCount = await Ticket.countDocuments({ status: 'Open', category });
-        const solvedCount = await Ticket.countDocuments({ status: 'Resolved', category });
-
-        stats.push({
-            category,
+        return {
+            _id: category._id,
+            categoryName: category.categoryName,
+            categoryDescription: category.categoryDescription,
+            categoryPriority: category.categoryPriority,
             activeTickets: activeCount,
-            solvedTickets: solvedCount
-        });
-    }
-
+            resolveTickets: solvedCount,
+            sla:"90% on-time",
+            __v: category.__v // Include version key if needed
+        };
+    }));
     return stats;
 };
 
