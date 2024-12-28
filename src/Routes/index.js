9ac54f4e7fd7677e278ef158d05572dc4ba0a5aa
MidlajLevonTechno <muhammedmidlaj236@gmail.com>
2024-12-28 15:53:01 +0530

    jwt validation added

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -29,9 +29,22 @@ const ticketCategoryRouter = require('./Ticket/ticketCategoryRouter.js')
 const ticketReplyRouter = require('./Ticket/ticketReplyRouter.js')
 // const contactRouter = require("./contactRouter.js")
 const uploadVCard = require("./VCard/VCardRoute.js")
-const settingsRouter = require('./settingsRoutes.js') 
+const settingsRouter = require('./settingsRoutes.js'); 
+const { validateJwtToken } = require('../Middleware/validateJwtToken.js');
 
 const router = express.Router();
+
+// Apply validateJwtToken to all routes except login routes
+// router.use((req, res, next) => {
+//   console.log("originalUrl from validateJwtToken - ",req.originalUrl);
+//   if (
+//       req.originalUrl.startsWith("/api/v1/individual") ||
+//       req.originalUrl.startsWith("/api/v1/enterprise")
+//   ) {
+//       return next(); // Skip validation for /individual and /enterprise
+//   }
+//   validateJwtToken()(req, res, next); // Apply validation for other routes
+// });
 
 const defaultRoutes = [
   {
