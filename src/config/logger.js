89880095e5a81700cd9e3b5ce906ae9e59b5bfd7
Rajanit Nagajibhai Navapara <51814458+rajanitnavapara@users.noreply.github.com>
2025-01-07 14:15:14 +0530

    Logger (#153)
    * Added Logging
    
    * Added Log Routes

---
+++ src/config/logger.js
@@ -0,0 +1,52 @@
+const winston = require('winston');
+const { combine, timestamp, printf, splat, simple } = winston.format;
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    info.message += `\n${info.stack}`;
+  }
+  return info;
+});
+
+winston.addColors({
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  debug: 'blue',
+});
+
+require('dotenv').config();
+
+const colorizeFormat = process.env.NODE_ENV === 'development' ? winston.format.colorize() : winston.format.uncolorize();
+
+
+const printfFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+  format: combine(
+    enumerateErrorFormat(),
+    splat(),
+    timestamp(),
+    simple(),
+    colorizeFormat,
+    printfFormat,
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error'],
+    }),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+logger.stream = {
+  write: function(message) {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
