9a5c4ee0db59e9dd3caee7c6f2eb34145bfdbd01
rajanit_navapara9 <rajanitnavapara9999@gmail.com>
2025-02-06 16:46:18 +0530

    fix: referral logs commenented

--- src/services/Referral/referral.service.js
+++ src/services/Referral/referral.service.js
@@ -133,8 +133,7 @@ const registerInviteeByReferralCode = async (referralCode, inviteePhoneNo, invit
     ]).exec();
 
     // console.log("withDrawn : ", withDrawn);
-
-    if (withDrawn.length === 0) {
+    if(withDrawn.length === 0) {
         withDrawn.push({ total: 0 });
     }
     const coinsBalance = parseInt(totalCoins[0].total || 0) - parseInt(withDrawn[0].total);
@@ -224,34 +223,6 @@ const createCardByReferralCode = async (referralCode, inviteePhoneNo) => {
         }
         const coinsBalance = parseInt(totalCoins[0].total || 0) - parseInt(withDrawn[0].total);
     
-
-      const withDrawn = await WithdrawalRequest.aggregate([
-            {
-                $match: {
-                    userId: newReferral.referrer,
-                    status: "approved",
-                },
-            },
-            {
-                $group: {
-                    _id: null,
-                    coinsRedeemed: { $sum: '$amount' },
-                },
-            },
-            {
-                $project: {
-                    _id: 0,
-                    total: '$coinsRedeemed',
-                },
-            },
-        ]).exec();-
-    
-        console.log("withDrawn : ", withDrawn);
-    
-        if (withDrawn.length === 0) {
-            withDrawn.push({ total: 0 });
-        }
-        const coinsBalance = parseInt(totalCoins[0].total || 0) - parseInt(withDrawn[0].total);
 
     const userType = (await checkUserType(newReferral.referrer)).userType;
     // Update referrerId's coin balance
