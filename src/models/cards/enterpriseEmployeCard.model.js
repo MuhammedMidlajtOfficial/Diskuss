29b27bbac473d22efd6967c8504532f0b94d6382
rajanit_navapara9 <rajanitnavapara9999@gmail.com>
2025-02-20 16:11:40 +0530

    updated employee card counts

--- src/models/cards/enterpriseEmployeCard.model.js
+++ src/models/cards/enterpriseEmployeCard.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const EnterpriseEmployeeCardSchema = new mongoose.Schema({
   userId:{
-    type: mongoose.Schema.Types.ObjectId, ref: 'EnterpriseEmployee',
+    type: String,
     required : true
   },
   enterpriseId:{
