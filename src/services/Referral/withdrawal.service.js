941167e3a1995c8b2e3a576c7ae3d6fb78bd69de
MuhammedMidlajtOfficial <muhammedmidlaj236@gmail.com>
2025-03-11 13:15:33 +0530

    getAllWithdrawalRequests update

--- src/services/Referral/withdrawal.service.js
+++ src/services/Referral/withdrawal.service.js
@@ -1,4 +1,7 @@
+const { individualUserCollection } = require('../../DBConfig');
 const {WithdrawalRequest} = require('../../models/referral/referral.model');
+const enterpriseEmployeModel = require('../../models/users/enterpriseEmploye.model');
+const enterpriseUser = require('../../models/users/enterpriseUser');
 
 const createWithdrawalRequest = async (userId, amount, upiId) => {
     const withdrawalRequest = new WithdrawalRequest({ userId, amount, upiId });
@@ -11,8 +14,46 @@ const getWithdrawalRequestByUserId = async (userId) => {
 }
 
 const getAllWithdrawalRequests = async () => {
-    return await WithdrawalRequest.find();
-}
+    const withdrawals = await WithdrawalRequest.find().lean();
+
+    for (let withdrawal of withdrawals) {
+        let user = null;
+
+        // Check in Individual Users
+        let isIndividualExist = await individualUserCollection
+            .findOne({ _id: withdrawal.userId }, { username: 1, email: 1 })
+            .lean();
+        if (isIndividualExist) {
+            user = { type: "Individual", ...isIndividualExist };
+        }
+
+        // Check in Enterprise Users
+        if (!user) {
+            let isEnterpriseExist = await enterpriseUser
+                .findOne({ _id: withdrawal.userId }, { username: 1, email: 1 })
+                .lean();
+            if (isEnterpriseExist) {
+                user = { type: "Enterprise", ...isEnterpriseExist };
+            }
+        }
+
+        // Check in Enterprise Employees
+        if (!user) {
+            let isEnterpriseEmployeeExist = await enterpriseEmployeModel
+                .findOne({ _id: withdrawal.userId }, { username: 1, email: 1 })
+                .lean();
+            if (isEnterpriseEmployeeExist) {
+                user = { type: "EnterpriseEmployee", ...isEnterpriseEmployeeExist };
+            }
+        }
+
+        // Attach user details to withdrawal
+        withdrawal.user = user;
+    }
+
+    console.log(withdrawals);
+    return withdrawals;
+};
 
 const checkRequestPending = async (userId) => {
     return await WithdrawalRequest.find({ userId, status: 'pending' });
